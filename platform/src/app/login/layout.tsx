5293630facae8ba3197d7186f17be8b62288d574
eagleconsulting954-a11y <eagleconsulting954@gmail.com>
2026-08-22 19:46:28 -0400

    Noindex login routes

---
+++ platform/src/app/login/layout.tsx
@@ -0,0 +1,5 @@
+import { privateRouteMetadata } from '@/lib/privateRouteMetadata';
+
+export const metadata=privateRouteMetadata;
+
+export default function LoginLayout({children}:{children:React.ReactNode}){return children;}
